5ff444613894d588b6ab64b73ecc3744c8681c98
Thato0928 <113536929+Thato0928@users.noreply.github.com>
2022-09-14 23:23:37 +0200

    updated malaria.vb

--- GroupProject_Team41/GroupProject_Team41/Malaria.vb
+++ GroupProject_Team41/GroupProject_Team41/Malaria.vb
@@ -1,10 +1,19 @@
-﻿Option Strict On
+Option Strict On
 Option Explicit On
 Option Infer Off
 Public Class Malaria
     Inherits Disease
     Private _vaccines As Vaccine
-    Private _numSurvived As Integer
+    Private _numSurvived() As Integer
+    
+    'constructor
+     Public Sub New(name As String, t As String, size As Integer)
+        MyBase.New(name, t, size)
+        ReDim _numSurvived(size)
+        _vaccines = New Vaccine
+    End Sub
+    
+    'property methods
     Public Property vaccines() As Vaccine
         Get
             Return _vaccines
@@ -13,34 +22,93 @@ Public Class Malaria
             _vaccines = value
         End Set
     End Property
-    Public Property numsurvived() As Integer
+    Public Property numsurvived(i As Integer) As Integer
         Get
-            Return _numSurvived
+            Return _numSurvived(i)
 
         End Get
         Set(value As Integer)
-            _numSurvived = value
+            _numSurvived(i) = value
 
         End Set
     End Property
-    Public Sub New(name As String, nP As Integer, nT As Integer, nA As Integer, nS As Integer, nD As Integer, t As String)
-        MyBase.New(name, nP, nT, nA, nD, t)
-        _numSurvived = nS
-    End Sub
+    
+    'general methods
     Public Function DeathSurvivalRatio() As String
-        Dim min As Integer
+        Dim max As Integer
         Dim d, s As Integer
         Dim Ratio As String
-        min = numdied
-        If min > numsurvived Then
-            min = numsurvived
+        Dim totalDied, totalSurvived As Integer
+        For i As Integer = 1 To _numSurvived.Length - 1
+            totalDied += numdied(i)
+            totalSurvived += numsurvived(i)
+        Next i
+        max = totalDied
+
+        If max < totalSurvived Then
+            max = totalSurvived
         End If
-        d = CInt(numdied / min)
-        s = CInt(numsurvived / min)
+        d = CInt(totalDied / max)
+        s = CInt(totalSurvived / max)
         Ratio = CStr(d & ":" & s)
         Return Ratio
     End Function
+
     Public Overridable Function SurvivalRate() As Double
-        Return (_numSurvived / population) * 100
+        Dim rate As Double
+        Dim status1, status2 As String
+        Dim Totalinf As Integer
+        Dim Totalsurv As Integer
+        For i As Integer = 1 To _numSurvived.Length - 1
+            Totalinf += NumInfected(i)
+            Totalsurv += numsurvived(i)
+        Next i
+        rate = CDbl(Format(((Totalsurv / Totalinf) * 100), "0.##"))
+        status1 = "Total survived: " & CStr(Totalsurv)
+        status2 = "Survival Rate: " & CStr(rate)
+        Return CDbl(status1 & Environment.NewLine _
+            & status2)
+    End Function
+    Public Function Veffective() As Double
+        Return _vaccines.Veffectiveness()
+
+    End Function
+    Public Overrides Function Infectionrate(size As Integer) As Double
+        Return MyBase.infectionRate(size)
+    End Function
+    Public Overrides Function AvePeopleInfected(size As Integer) As Double
+        Return MyBase.AvePeopleInfected(size)
+    End Function
+    Public Overrides Function DeathRate() As Double
+        Return MyBase.DeathRate()
+    End Function
+
+    Public Overrides Function yearEval(size As Integer) As String
+        Dim max As Integer
+        Dim Rate As String
+        max = numsurvived(1)
+        If max < numsurvived(size) Then
+            Rate = "Increasing"
+        Else
+            Rate = "Decreasing"
+        End If
+        Return MyBase.yearEval(size) + " " & Environment.NewLine _
+        & "Survival rate:" & Rate
+    End Function
+
+    Public Overrides Function Display(size As Integer) As String
+        Dim totalsurv As Integer
+        For i As Integer = 1 To size
+            totalsurv += numsurvived(i)
+        Next
+        Return MyBase.Display(size) & Environment.NewLine _
+            & "Number of Cured: " & CStr(totalsurv) & Environment.NewLine _
+         & "Survival Rate: " & Format(SurvivalRate(), "0.##") & Environment.NewLine _
+             & "Death-Survival Ratio" & " " & DeathSurvivalRatio() & Environment.NewLine _
+              & "Vaccine Name: " & vaccines.name & Environment.NewLine _
+               & "Number of doses: " & CStr(vaccines.doses) & Environment.NewLine _
+               & "Number of Vaccinated:" & CStr(vaccines.vaccinated) & Environment.NewLine _
+                & "Number of Infected while Vaccinated:" & CStr(vaccines.infectedvaccinated) & Environment.NewLine _
+         & "Vaccine effectiveness:" & Format(Veffective(), "0.##")
     End Function
 End Class
